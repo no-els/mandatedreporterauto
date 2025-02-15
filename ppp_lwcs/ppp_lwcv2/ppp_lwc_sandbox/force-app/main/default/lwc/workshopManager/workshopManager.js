94e03e3ffab2663c7ea42a3ba1a8963274b32f9c
Noel Ngui <noelngui@MG-LMQF7NPNYV.localdomain>
2025-02-14 17:58:57 -0800

    moved attendees to workshop manager

--- ppp_lwcs/ppp_lwcv2/ppp_lwc_sandbox/force-app/main/default/lwc/workshopManager/workshopManager.js
+++ ppp_lwcs/ppp_lwcv2/ppp_lwc_sandbox/force-app/main/default/lwc/workshopManager/workshopManager.js
@@ -1,15 +1,30 @@
-import { LightningElement , api } from 'lwc';
+import { LightningElement , api , wire } from 'lwc';
+import getAttendees from '@salesforce/apex/WorkshopController.getAttendees';
 
 export default class WorkshopManager extends LightningElement {
     selectedWorkshopId="None";
     selectedWorkshopName="None";
     selectedWorkshopSite="None";
     selectedWorkshopDate="None";
+    attendees;
+    wiredAttendees;
+
+    @wire(getAttendees, { workshopId: '$selectedWorkshopId' })
+    wiredAttendeesData(result) {
+        this.wiredAttendees = result;
+        if (result.data) {
+            this.attendees = result.data;
+        }
+    }
     handleWorkshopSelect(event) {
         this.selectedWorkshopId = event.detail.id;
         this.selectedWorkshopName = event.detail.name;
         this.selectedWorkshopSite = event.detail.site;
         this.selectedWorkshopDate = event.detail.date;
         console.log('asdf',  this.selectedWorkshopSite)
+    }
+    refreshData() {
+        console.log("Refreshing attendees list...");
+        return refreshApex(this.wiredAttendees);
     }
 }
